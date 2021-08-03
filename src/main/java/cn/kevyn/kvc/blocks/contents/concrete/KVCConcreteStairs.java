ed5a79f7d633959d55ad0ea32270611421c909b1
Kevyn <532775134@qq.com>
2021-08-04 00:32:22 +0800

    Update KVCConcreteStairs.java

--- src/main/java/cn/kevyn/kvc/blocks/contents/concrete/KVCConcreteStairs.java
+++ src/main/java/cn/kevyn/kvc/blocks/contents/concrete/KVCConcreteStairs.java
@@ -3,13 +3,17 @@ package cn.kevyn.kvc.blocks.contents.concrete;
 import cn.kevyn.kvc.blocks.patches.AllowSpawning;
 import cn.kevyn.kvc.blocks.patches.RegisterHelper;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.*;
 
 
 public class KVCConcreteStairs {
 
     private static BlockState CONCRETE_BLOCKSTATE = Blocks.BLACK_CONCRETE.getDefaultState();
-    private static AbstractBlock.Settings CONCRETE_STAIRS_SETTINGS = FabricBlockSettings.copy(Blocks.BLACK_CONCRETE).allowsSpawning(AllowSpawning::never);
+    private static AbstractBlock.Settings CONCRETE_STAIRS_SETTINGS = FabricBlockSettings.copyOf(Blocks.BLACK_CONCRETE)
+            .allowsSpawning(AllowSpawning::never)
+            .breakByTool(FabricToolTags.PICKAXES)
+            .requiresTool();
 
     public static void doRegister() {
 
